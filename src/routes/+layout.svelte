4d8d4a88426919872e25896f0fce1c378b5e5a8b
paulweezydesign <weezy@paulweezydesign.dev>
2023-01-01 23:51:28 +0000

    added about page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,15 @@
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
 </script>
+
+<nav class="flex justify-evenly text-xl py-4">
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/kittens">Kittens</a>
+	<a href="/resources">Resources</a>
+	<a href="/contact">Contact</a>
+	
+</nav>
 <div class="layout">
 <slot />
 </div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<div class=" h-screen w-screen layout">
+	<div class=" px-4">
+	<h1 class="text-6xl sm:text-7xl font-black">About us</h1>
+    <div class="columns-3 gap-4 prose-xl lg:prose-2xl"></div>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum sit suscipit aliquam facere, mollitia laborum provident officia veniam eaque, est illum dolor sunt? Voluptatibus cupiditate animi quisquam itaque reiciendis. Voluptatum amet blanditiis suscipit quo esse enim quibusdam, sunt voluptatibus cupiditate cum excepturi eligendi eius accusamus eaque architecto delectus debitis expedita?
+      
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum sit suscipit aliquam facere, mollitia laborum provident officia veniam eaque, est illum dolor sunt? Voluptatibus cupiditate animi quisquam itaque reiciendis. Voluptatum amet blanditiis suscipit quo esse enim quibusdam, sunt voluptatibus cupiditate cum excepturi eligendi eius accusamus eaque architecto delectus debitis expedita?
+     
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt similique tempora asperiores enim doloribus esse?
+    </div>
+
+</div>
