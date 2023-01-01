f2391a034df58892c3f9b318f03a62c9609854b9
paulweezydesign <86060999+paulweezydesign@users.noreply.github.com>
2023-01-01 22:57:46 +0000

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import "../theme.postcss"
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+</script>
+<div class="layout">
+<slot />
+</div>
+
+
+<style global>
+
+	.layout {
+		@apply container h-full mx-auto flex justify-center items-center;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+let	name = "weezy"
+</script>
+
+<div class=" h-screen w-screen layout">
+	<div class="max-w-4xl px-4">
+	<h1 class="text-6xl sm:text-7xl md:text-8xl font-black">Come adopt my <span class="text-primary-500">cute</span> ass <span class="text-tertiary-500">kittens</span></h1>
+	<p class="prose-xl lg:prose-2xl mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam praesentium nesciunt eius nobis eos numquam rem inventore corrupti delectus quo.</p>
+	
+	<div class="mt-4 flex flex-col md:flex-row">
+		
+		<a href="/about" class="btn btn-filled-primary btn-base">Learn More</a>
+		<span class='py-3 md:px-2' />
+		<a href="/kittens" class="btn btn-filled-tertiary  btn-base">To Cuteness</a>
+	
+	</div>
+	
+	
+
+
+
+</div>
+</div>
+<style>
+	.div1 {
+		
+background-color:hsla(309,100%,50%,1);
+background-image:
+radial-gradient(at 40% 20%, hsla(132,100%,37%,1) 0px, transparent 50%),
+radial-gradient(at 80% 0%, rgb(166, 88, 214) 0px, transparent 50%),
+radial-gradient(at 0% 50%, hsla(217,100%,38%,1) 0px, transparent 50%),
+radial-gradient(at 80% 50%, hsla(312,100%,60%,1) 0px, transparent 50%),
+radial-gradient(at 0% 100%, hsla(122,100%,77%,1) 0px, transparent 50%),
+radial-gradient(at 80% 100%, hsla(242,100%,70%,1) 0px, transparent 50%),
+radial-gradient(at 0% 0%, hsla(343,100%,76%,1) 0px, transparent 50%);
+
+	}
+	
+</style>
